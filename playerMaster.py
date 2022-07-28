2808383c1344415dbdb894a142318068ce8e8c12
hcung <hcung@sfu.ca>
2022-07-28 00:59:00 -0700

    Add functions: lockEgg(), occupyEgg(), rmvDisconnectPlayer(), sendMsgToPlayer(), prepNotification()

--- playerMaster.py
+++ playerMaster.py
@@ -9,15 +9,18 @@ import threading
 
 # playerThread - child of class Threading 
 class playerThread(threading.Thread):
-	# eggHolding - keep the egg object that players is currently working on
-	EGGHOLDING = None
+	# playerThread configuration
 	CLIENTSOCKET = None
 	CLIENTADDRESS = None
 	BUF_SIZE = 1024
-	DICT_EGG = None
+	MUTEX = None
+
+	# Gameplay properties
+	EGGHOLDING = None 	# EGGHOLDING - keep the egg object that players is currently working on
+	DICT_EGG = None 	# DICT_EGG - reference of DICT_EGG from server
 
 	# Constructor
-	def __init__(self, threadID, threadName, clientSocket, clientAddress, bufferSize, dictEggObj):
+	def __init__(self, threadID, threadName, clientSocket, clientAddress, bufferSize, dictEggObj, mutex):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 		self.name = threadName
@@ -25,6 +28,7 @@ class playerThread(threading.Thread):
 		self.CLIENTADDRESS = clientAddress 
 		self.BUF_SIZE = bufferSize
 		self.DICT_EGG = dictEggObj
+		self.MUTEX = mutex
 
 	# run() - override run() -> trigger the thread to start
 	def run(self):
@@ -38,55 +42,95 @@ class playerThread(threading.Thread):
 			if not msgData:
 				#disconnect
 				print("{} disconnect from server".format(self.threadID))
+				#need to clean everything after the player get disconnected
+				#write the code here
 				break
 
 			# valid request:
 			else:
-				print("{}: {}".format(self.threadID, msgData))
+				#print("{}: {}".format(self.threadID, msgData))
+				#react based on the client's request
+				#write the code here
+		return None
 
-		return None	
+	# lockEgg() - clients requests to lock an egg
+	# Client acquire key -> to update the egg.lockState & egg.ownerID
+	# Successfully lock -> True; otherwise -> False
+	def lockEgg(self, eggCoordinate):
+		# trigger notification flag
+		lockSuccess = False
+
+		# Obtain the key - to access DICT_EGG - timeout to 1 second -> prevent deadlock
+		self.MUTEX.acquire(1)
+		
+		# Access the egg 
+		egg = self.DICT_EGG[eggCoordinate]
+
+		# Use "try" and "finally" to guarantee action
+		# If egg is not being locked/occupied by anyone 
+		# -> allow to lock the egg -> trigger notification of success/failure
+		try:
+			if egg.isLock() == False and egg.isOccupied == False:
+				# Switch lockState to True:
+				egg.setLock(True)
+				egg.setEggOwnerID = self.threadID
+
+				# hold the egg in EGGHOLDING - Prevent deadlock:
+				self.EGGHOLDING = egg
+				lockSuccess = True
+			
+		# Release the key for next user:
+		finally:
+			self.MUTEX.release()
+
+		return lockSuccess
 
 
-	# # handleClientCommunication() - this will be a separate thread for each clients connect to server
- #    def handleClientCommunication(conn, player_num):
- #        global ready_count, player_count
- #        conn.send(str.encode("Connection established"))
- #        conn.recv(BUF_SIZE)
- #        ready_count += 1
-     
- #        # busy wait -- there is definitely a better solution
- #        # or just remove the feature of waiting for everyone to join
- #        while self.READY_PLAYERS != self.CONNECTED_PLAYERS:
- #            time.sleep(0.5)
+	# occupyEgg() - clients request to occupy an egg
+	# Client acquire key -> to update the egg.occupyState & egg.ownerID
+	# Successfully occupy -> True; otherwise -> False
+	def occupyEgg(self, eggCoordinate):
+		#trigger notification flag
+		occupySuccess = False
 
- #        # assigns player num, client uses to determine other clients data
- #        conn.send(str.encode(str(player_num)))
+		# Obtain the key - to access DICT_EGG - timeout to 1 second -> prevent deadlock
+		self.MUTEX.acquire(1)
 
- #        while True:
- #            data = conn.recv(BUF_SIZE)
- #            msg = data.decode()
-            
- #            # if someone leaves, make room for another person to join
- #            # could probably be handled better -- quite buggy atm
- #            if not data:
- #                print("Disconnecting...")
- #                ready_count -= 1
- #                player_count -= 1 
- #                break
- #            else:
- #                ##### This part need to be reviewed ######
- #                # protocols for client info
- #                if msg == "MOUSE":
- #                    # sends clients other clients cursor coordinates
- #                    conn.send(str.encode("ready for coords from player " + str(player_num)))
- #                    data = conn.recv(BUF_SIZE)
- #                    coords = read_coords(data.decode())
- #                    mouse_coords[player_num] = coords
- #                    conn.send(str.encode(encode_coords(mouse_coords,"mouse")))
+		# Access the egg
+		egg = self.DICT_EGG[eggCoordinate]
 
- #                # sends all clients egg coordinates
- #                elif msg == "EGG":
- #                    conn.send(str.encode(encode_coords(egg_coords,'egg')))
+		# If egg is being locked by the same person who occupy
+		# -> allow to occupy the egg -> trigger notification of success/failure
+		try:
+			if egg.isLock() == True and egg.getOwnerID() == self.threadID:
+				#Switch occupyState to True & visibleState to False:
+				egg.setOccupy(True)
+				egg.setVisible(False)
 
- #        print("Connection lost...")
- #        conn.close()	
+				#release EGGHOLDING -> for the next egg:
+				self.EGGHOLDING = None
+				occupySuccess = True
+
+		# Relaese the key for the next user
+		finally:
+			self.MUTEX.release()
+
+		return occupySuccess
+
+	# rmvDisconnectPlayer - free the thread for next user (reuse)
+	def rmvDisconnectPlayer(self):
+		pass
+
+	# sendMsgToPlayer() - send message back to clients
+	def sendMsgToPlayer(self, msgContent):
+		pass
+
+	# prepNotification() - prepare notification message (send to clients)
+	def prepNotification(self, typeNotify, onObject):
+		prepMsg = ""
+
+		# typeNotify is True -> Success Notification
+
+		# typeNotify is False -> Failure Notification
+
+		return prepMsg
