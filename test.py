3c76529fe4ebc1c0a2e69fd27de3f224ff03efaf
hcung <hcung@sfu.ca>
2022-07-30 12:38:36 -0700

    update test file

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import eggMaster
 import playerMaster
+import server_TCP
 import socket
+import json
 
 #Socket setup
-SERVERADDRESS = "127.1.1.1"
-SERVERPORT = 1234
-SERVERBUF_SIZE = 1024
+SERVERADDRESS = "127.0.0.1"
+SERVERPORT = 16543
+SERVERBUF_SIZE = 2098
 FORMAT = "json"
-MAX_NUM_PLAYERS = 2
+MAX_NUM_PLAYERS = 4
 CONNECTED_PLAYERS = 0
 
 #Egg setup
@@ -15,41 +17,29 @@ DICT_EGGS_OBJ = {}
 PLAYER_THREAD = []
 GAME_LIVE = True
 
-def testDict():
-	eggAdminTest = eggMaster.eggAdmin("Egg1","Admin1", True, DICT_EGGS_OBJ)
-	print(eggAdminTest.THREADID)
-	print(eggAdminTest.THREADNAME)
-	eggAdminTest.run(GAME_LIVE, 10)
+class SimpleObj:
+	countryName = ""
+	continentName = ""
+
+	def __init__(self, country, continent):
+		self.countryName = country
+		self.continentName = continent
+
+def testServer_TCPClass():
+	print("Work outside")
+	server = server_TCP.ServerRoom(SERVERADDRESS, SERVERPORT, SERVERBUF_SIZE, MAX_NUM_PLAYERS)
+	server.start()
 	return None
 
-def testSocket():
-	serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-		serverSock.bind((SERVERADDRESS, SERVERPORT))
-	except socket.error as socketSetupError:
-		str(socketSetupError)
-
-	serverSock.listen(MAX_NUM_PLAYERS)
-	count = 1
-	while len(PLAYER_THREAD) < MAX_NUM_PLAYERS:
-		print("Server is up")
-		print(len(PLAYER_THREAD))
-		clientSock, clientAddress = serverSock.accept()
-		print("{} has connected".format(clientAddress))
-		clientHelper = playerMaster.playerThread(count, "player", clientSock, clientAddress, 4096)
-		clientHelper.start()
-		PLAYER_THREAD.append(clientHelper)
-		count += 1
-	return None
 
 def main(): 
-	testDict()
-	for men in (DICT_EGGS_OBJ):
-		print(men.getCoordinate())
-	# testSocket()
-	# for threads in PLAYER_THREAD:
-	# 	threads.join()
-	# return None
+	#testDict()
+	#for men in (DICT_EGGS_OBJ):
+	#	print(men.getCoordinate())
+	testServer_TCPClass()
+	for threads in PLAYER_THREAD:
+		threads.join()
+	return None
 
 
 if __name__ == "__main__":
