06629241f013a39afa28bf09523354c6a4afb33a
hcung <hcung@sfu.ca>
2022-07-31 22:01:28 -0700

    latest update

--- test.py
+++ test.py
@@ -25,6 +25,20 @@ class SimpleObj:
 		self.countryName = country
 		self.continentName = continent
 
+def decodeMsgToObject(userMsg):
+	decodeMsg = json.loads(userMsg)
+
+	# if decodeMsg['OBJECTTYPE'] =="Egg":
+	# 	decodeMsg['DATA'] = json.loads(decodeMsg["DATA"])
+	return decodeMsg
+
+def testDecodeJson():
+	jsonMsg = '{"REQUESTTYPE": "data", "OBJECTTYPE": "Egg", "DATA": "{"xCoordinate": "1","yCoordinate": "2"}"}'
+	loadMsg = decodeMsgToObject(jsonMsg)
+	print(loadMsg['REQUESTTYPE'])
+	print(loadMsg['OBJECTTYPE'])
+	print(loadMsg['DATA'])
+
 def testServer_TCPClass():
 	print("Work outside")
 	server = server_TCP.ServerRoom(SERVERADDRESS, SERVERPORT, SERVERBUF_SIZE, MAX_NUM_PLAYERS)
@@ -33,9 +47,10 @@ def testServer_TCPClass():
 
 
 def main(): 
-	testServer_TCPClass()
-	for threads in PLAYER_THREAD:
-		threads.join()
+	# testServer_TCPClass()
+	# for threads in PLAYER_THREAD:
+	# 	threads.join()
+	testDecodeJson()
 	return None
 
 
